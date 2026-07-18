2346b45fa8c4ba14250dc6272a1065bb292766d6
Manikandan R <manirajv06@gmail.com>
2026-07-18 18:44:35 +0530

    [addendum pr] rename api

--- lib/go/api/interface.go
+++ lib/go/api/interface.go
@@ -51,11 +51,11 @@ type ResourceManagerCallback interface {
 	//Receive Node Update Response
 	UpdateNode(response *si.NodeResponse) error
 
-	// PredicatesPreFilter Run a certain set of pre-filter functions to find the feasible nodes
+	// PreFilterPredicates Run a certain set of pre-filter functions to find the feasible nodes
 	// for the given pod. If all nodes are eligible, empty map along with nil error would be returned.
 	// Otherwise, map containing only feasible nodes along with nil error would be returned. Non-nil error
 	// would be returned for all other failure cases.
-	PredicatesPreFilter(args *si.PredicatesArgs) (map[string]struct{}, error)
+	PreFilterPredicates(args *si.PreFilterPredicatesArgs) *si.PreFilterPredicatesResponse
 
 	// Run a certain set of predicate functions to determine if a proposed allocation
 	// can be allocated onto a node.
